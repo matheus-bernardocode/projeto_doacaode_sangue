59204f7a57802dfc4df99d8486dad6f8a3aac8ed
matheus_ruekl <matheus_ruekl@estudante.sesisenai.org.br>
2026-05-13 15:27:50 -0300

    ADD- Adicionei as validacoes iniciais do js.

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 let doadores = [];
 
-document.getElementById("formDoacao").addEventListener("submit", function(e) {
+document.getElementById("formdoacao").addEventListener("submit", function(e) {
     e.preventDefault();
 
     let nome = document.getElementById("nome").value.trim();
@@ -11,3 +11,56 @@ document.getElementById("formDoacao").addEventListener("submit", function(e) {
     let telefone = document.getElementById("telefone").value.trim();
     let cidade = document.getElementById("cidade").value.trim();
     let estado = document.getElementById("estado").value.trim();
+
+    //VALIDAÇÕES
+
+    //Obrigatórios
+    if (
+        nome === "" ||
+        email === "" ||
+        !idade ||
+        !peso ||
+        tipoSanguineo === "" ||
+        telefone === "" ||
+        cidade === "" ||
+        estado === ""
+    ) {
+        alert("Todos os campos são obrigatórios.");
+        return;
+    }
+
+    //Nome e sobrenome
+    if (nome.split(" ").length < 2) {
+        alert("Digite nome e sobrenome.");
+        return;
+    }
+
+    //Email válido
+    let regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!regexEmail.test(email)) {
+        alert("Digite um email válido.");
+        return;
+    }
+
+    //Idade mínima
+    if (idade < 16) {
+        alert("A idade mínima é 16 anos.");
+        return;
+    }
+
+    //Peso mínimo
+    if (peso < 50) {
+        alert("O peso mínimo é 50kg.");
+        return;
+    }
+
+    //Telefone com apenas números
+    let regexTelefone = /^[0-9]+$/;
+
+    if (!regexTelefone.test(telefone)) {
+        alert("O telefone deve conter apenas números.");
+        return;
+    }
+
+});
